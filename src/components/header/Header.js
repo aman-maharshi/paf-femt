93f04ca262011fe54e1504e37d037d06851c89ae
aman-maharshi <amanm4harshi@gmail.com>
2023-09-25 19:44:13 +0530

    header and tailwind init

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <div className='p-4 flex justify-center items-center bg-slate-800 text-white'>
+        <div className='flex items-center gap-4'>
+          <div className='bg-red-500 h-4 w-4 border rounded-full'></div>
+          Get access to all 350+ video series at once! 
+        </div>
+        <button className='px-3 pb-[2px] border border-white rounded-md ml-4 text-base'>Click Here</button>
+      </div>
+    </>
+  )
+}
+
+export default Header
