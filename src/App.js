5cf6ea38825f5779882b66349aa5fa30602bcaa1
aman-maharshi <amanm4harshi@gmail.com>
2023-09-28 15:55:41 +0530

    courses api integration

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+
 import Faq from "./components/Faq";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
@@ -6,12 +8,34 @@ import VideoSeriesTwo from "./components/VideoSeriesTwo";
 import Header from "./components/header";
 
 function App() {
+  const [courses, setCourses] = useState([])
+  const [relatedContent, setRelatedContent] = useState([])
+
+  useEffect(() => {
+    getCoursesData()
+  }, [])
+
+  const getCoursesData = async () => {
+    try {
+      const response = await fetch('https://api.acharyaprashant.org/v2/legacy/courses/series/optuser/course-series-eeb9d3')
+      if (response.ok) {
+        const data = await response.json()
+        console.log(data)
+        setCourses(data?.courses)
+        setRelatedContent(data?.relatedContent)
+      } 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+
   return (
     <div>
       <Header />
       <Hero />
-      <VideoSeriesOne />
-      <VideoSeriesTwo />
+      <VideoSeriesOne data={courses} />
+      <VideoSeriesTwo data={relatedContent} />
       <Faq />
       <Footer />
     </div>
--- src/components/VideoSeriesTwo.js
+++ src/components/VideoSeriesTwo.js
@@ -1,47 +1,25 @@
 import React from 'react'
 
-function VideoSeriesTwo() {
+function VideoSeriesTwo({ data }) {
   return (
     <div className='lg:px-8 px-4 py-4 mt-4'>
       <h2 className='text-[20px] font-medium pb-4 border-b border-gray-300'>Other Helpful Video Series</h2>
 
       <div className='grid md:grid-cols-2 grid-cols-1 gap-4'>
-        <div className='flex gap-4 p-4 cursor-pointer hover:bg-slate-100 border-b border-gray-200 rounded-md'>
-          <div className='w-[32%] h-[125px] bg-gray-200 rounded-md'></div>
-          <div>
-            <h3 className='text-[18px] mb-1'>Kabir Sahab</h3>
-            <div className='text-gray-500 mb-1'>Based on Selected Verses of Kabir Saheb</div>
-            <div className='text-gray-500 text-[14px]'>4 Video Series</div>
-          </div>
-        </div>
 
-        <div className='flex gap-4 p-4 cursor-pointer hover:bg-slate-100 border-b border-gray-200 rounded-md'>
-          <div className='w-[32%] h-[125px] bg-gray-200 rounded-md'></div>
-          <div>
-            <h3 className='text-[18px] mb-1'>Kabir Sahab</h3>
-            <div className='text-gray-500 mb-1'>Based on Selected Verses of Kabir Saheb</div>
-            <div className='text-gray-500 text-[14px]'>4 Video Series</div>
-          </div>
-        </div>
-
-        <div className='flex gap-4 p-4 cursor-pointer hover:bg-slate-100 border-b border-gray-200 rounded-md'>
-          <div className='w-[32%] h-[125px] bg-gray-200 rounded-md'></div>
-          <div>
-            <h3 className='text-[18px] mb-1'>Kabir Sahab</h3>
-            <div className='text-gray-500 mb-1'>Based on Selected Verses of Kabir Saheb</div>
-            <div className='text-gray-500 text-[14px]'>4 Video Series</div>
-          </div>
-        </div>
-
-        <div className='flex gap-4 p-4 cursor-pointer hover:bg-slate-100 border-b border-gray-200 rounded-md'>
-          <div className='w-[32%] h-[125px] bg-gray-200 rounded-md'></div>
-          <div>
-            <h3 className='text-[18px] mb-1'>Kabir Sahab</h3>
-            <div className='text-gray-500 mb-1'>Based on Selected Verses of Kabir Saheb</div>
-            <div className='text-gray-500 text-[14px]'>4 Video Series</div>
-          </div>
-        </div>
-
+        {data.map(item => {
+          const {id, title, subtitle, thumbnail} = item
+          return (
+            <div key={id} className='grid grid-cols-3 gap-4 p-4 cursor-pointer hover:bg-slate-100 border-b border-gray-200 rounded-md'>
+              <div className='col-span-1 h-[100px] bg-gray-200 rounded-md'></div>
+              <div className='col-span-2'>
+                <h3 className='text-[18px] mb-1'>{title}</h3>
+                <div className='text-gray-500 mb-1'>{subtitle}</div>
+                <div className='text-gray-500 text-[14px]'>4 Video Series</div>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
